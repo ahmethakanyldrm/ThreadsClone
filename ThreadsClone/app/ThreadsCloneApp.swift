e40485f7b455c6c106bf79e87f1c91bfb7d85466
ahmethakanyldrm <65174957+ahmethakanyldrm@users.noreply.github.com>
2024-01-23 23:22:33 +0300

    init project

---
+++ ThreadsClone/app/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by AHMET HAKAN YILDIRIM on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThreadsClone/core/root/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ThreadsClone
+//
+//  Created by AHMET HAKAN YILDIRIM on 23.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                .font(.callout)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
