7ae8fb34162eca5b911b54d3a68e56ed4a243470
ahmethakanyldrm <65174957+ahmethakanyldrm@users.noreply.github.com>
2024-01-24 18:05:09 +0300

    feed view UI added

---
+++ ThreadsClone/core/feed/view/FeedView.swift
@@ -0,0 +1,45 @@
+//
+//  FeedView.swift
+//  ThreadsClone
+//
+//  Created by AHMET HAKAN YILDIRIM on 24.01.2024.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(0 ... 10, id: \.self) { thread in
+                    
+                        ThreadCell()
+                        
+                    }
+                }
+            }
+            .refreshable {
+                print("DEBUG: Refresh thread")
+            }
+            .navigationTitle("Threads")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.counterclockwise")
+                        .foregroundStyle(.black)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        FeedView()
+    }
+}
---
+++ ThreadsClone/core/profile/view/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  ThreadsClone
+//
+//  Created by AHMET HAKAN YILDIRIM on 24.01.2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
